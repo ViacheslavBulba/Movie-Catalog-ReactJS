880370512cbc8a7e810c17b2b36c047f944bb5e9
viacheslav.bulba@verizon.com <viacheslav.bulba@verizon.com>
2021-02-26 17:50:09 -0800

    removed empty lines and comments

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,18 +1,11 @@
-// Webpack uses this to work with directories
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-// This is the main configuration object.
-// Here you write different options and tell Webpack what to do
 module.exports = {
-
-    // Path to your entry point. From this file Webpack will begin it's work
     entry: './src/index.js',
-
     plugins: [
-        // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({ template: './src/index.html' }),
         new MiniCssExtractPlugin({
@@ -20,8 +13,6 @@ module.exports = {
             chunkFilename: '[id].css',
         }),
     ],
-
-    // In module->rules we can tell Webpack how to transform different types of files
     module: {
         rules: [
             {
@@ -34,24 +25,18 @@ module.exports = {
                     }
                 }
             },
-
             {
-
                 test: /\.((c|sa|sc)ss)$/i,
                 use: [
                     "style-loader",
                     {
                         loader: "css-loader",
                         options: {
-                            // Run `postcss-loader` on each CSS `@import`, do not forget that `sass-loader` compile non CSS `@import`'s into a single file
-                            // If you need run `sass-loader` and `postcss-loader` on each CSS `@import` please set it to `2`
                             importLoaders: 1,
                         },
                     },
-
                 ],
             },
-
             {
                 test: /\.svg$/,
                 use: [
@@ -71,7 +56,6 @@ module.exports = {
                     },
                 ],
             },
-
             {
                 test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
                 use: [
@@ -86,7 +70,6 @@ module.exports = {
             }
         ]
     },
-
     output: {
         path: path.resolve(__dirname, '../dist'),
         filename: '[name].bundle.js',
