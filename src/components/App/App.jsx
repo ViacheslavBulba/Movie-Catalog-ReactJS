c9b953c954446a58b543c7dc31c1531b23585c15
viacheslav.bulba@verizon.com <viacheslav.bulba@verizon.com>
2021-04-12 21:48:24 -0700

    logic for landing on film id moved to OverviewView

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,8 @@
 import './App.css';
 
-import axios from 'axios';
-import config from 'config';
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { Route, Switch, useLocation } from 'react-router-dom';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-import { setMovieNotFoundById, setMovieToOverview } from '../../store/actions';
-import store from '../../store/store';
 import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 import Footer from '../Footer/Footer';
 import OverviewView from '../OverviewView/OverviewView';
@@ -15,31 +10,6 @@ import PageNotFound from '../PageNotFound/PageNotFound';
 import SearchView from '../SearchView/SearchView';
 
 export default function App() {
-    const movieNotFound = useSelector((state) => state.movieNotFoundById);
-
-    let location = useLocation();
-
-    const checkPathOnLanding = () => {
-        const overviewRegExp = /^\/film\/(\d+)$/i;
-        if (overviewRegExp.test(location.pathname)) {
-            const found = location.pathname.match(overviewRegExp);
-            const movieId = found[1];
-            axios
-                .get(`${config.apiUrl}/movies/${movieId}`)
-                .then((response) => {
-                    store.dispatch(setMovieToOverview(response.data));
-                })
-                .catch(() => {
-                    store.dispatch(setMovieNotFoundById(true));
-                });
-            return;
-        }
-    };
-
-    useEffect(() => {
-        checkPathOnLanding();
-    }, []);
-
     return (
         <ErrorBoundary>
             <Switch>
@@ -47,7 +17,7 @@ export default function App() {
                     <SearchView />
                 </Route>
                 <Route path={'/film/:id'} exact>
-                    {movieNotFound ? <PageNotFound /> : <OverviewView />}
+                    <OverviewView />
                 </Route>
                 <Route path='*'>
                     <PageNotFound />
--- src/components/OverviewView/OverviewView.jsx
+++ src/components/OverviewView/OverviewView.jsx
@@ -1,23 +1,56 @@
-import React from 'react';
+import axios from 'axios';
+import config from 'config';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
+import { setMovieNotFoundById, setMovieToOverview } from '../../store/actions';
+import store from '../../store/store';
 import Filtering from '../Filtering/Filtering';
-import Sorting from '../Sorting/Sorting';
-import ResultsCount from '../ResultsCount/ResultsCount';
 import MovieList from '../MovieList/MovieList';
 import MovieOverview from '../MovieOverview/MovieOverview';
+import PageNotFound from '../PageNotFound/PageNotFound';
+import ResultsCount from '../ResultsCount/ResultsCount';
+import Sorting from '../Sorting/Sorting';
 
 export default function OverviewView() {
+    let { id } = useParams();
+
+    const movieNotFound = useSelector((state) => state.movieNotFoundById);
+
+    const checkPathOnLanding = () => {
+        axios
+            .get(`${config.apiUrl}/movies/${id}`)
+            .then((response) => {
+                store.dispatch(setMovieToOverview(response.data));
+            })
+            .catch(() => {
+                store.dispatch(setMovieNotFoundById(true));
+            });
+    };
+
+    useEffect(() => {
+        checkPathOnLanding();
+    }, []);
+
     return (
         <>
-            <MovieOverview />
-            <div className='divider' />
-            <main className='main-container'>
-                <div className='filtering-and-sorting-container'>
-                    <Filtering />
-                    <Sorting />
-                </div>
-                <ResultsCount />
-                <MovieList />
-            </main>
+            {movieNotFound ? (
+                <PageNotFound />
+            ) : (
+                <>
+                    <MovieOverview />
+                    <div className='divider' />
+                    <main className='main-container'>
+                        <div className='filtering-and-sorting-container'>
+                            <Filtering />
+                            <Sorting />
+                        </div>
+                        <ResultsCount />
+                        <MovieList />
+                    </main>
+                </>
+            )}
         </>
     );
 }
