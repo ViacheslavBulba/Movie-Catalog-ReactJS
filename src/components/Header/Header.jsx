cbf2a53e20e572995d6eada14135bca93b2f7b1c
bulbavi <viacheslav.bulba@verizon.com>
2021-09-14 21:38:03 -0700

    reduced height of header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -39,30 +39,32 @@ export default function Header() {
 
     return (
         <header className='header-container'>
-            <div className='logo-and-add-button-container'>
+
+            <div className='logo-container'>
                 <img src={logo} alt='logo' className='logo' />
-                <button className='add-button' onClick={showAddMovieModal}>
-                    ADD MOVIE
-                </button>
-                <MovieDetailsModal
-                    show={showModal}
-                    handleCloseModal={handleCloseModal}
-                />
             </div>
-            <div className='header-text'>FIND YOUR MOVIE</div>
-            <div className='search-container'>
-                <input
-                    type='text'
-                    placeholder='What do you want to watch?'
-                    className='search-input'
-                    value={searchValue}
-                    onChange={(e) => setSearchValue(e.target.value)}
-                    onKeyPress={doSearchIfEnterKeyPressed}
-                />
-                <button className='search-button' onClick={handleSearch}>
-                    SEARCH
-                </button>
-            </div>
+
+            <input
+                type='text'
+                placeholder='What do you want to watch?'
+                className='search-input'
+                value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)}
+                onKeyPress={doSearchIfEnterKeyPressed}
+            />
+
+            <button className='search-button' onClick={handleSearch}>
+                SEARCH
+            </button>
+
+            <button className='add-button' onClick={showAddMovieModal}>
+                ADD MOVIE
+            </button>
+            <MovieDetailsModal
+                show={showModal}
+                handleCloseModal={handleCloseModal}
+            />
+
         </header>
     );
 }
